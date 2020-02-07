50cbc42e34c822467946c0b88aba166e882b938c
Ethan Griffee <griffee.e@husky.neu.edu>
2020-02-07 14:36:28 -0500

    changed column/dataframe a little

--- assignment3/part2/DataFrame.h
+++ assignment3/part2/DataFrame.h
@@ -7,18 +7,32 @@
 #include <cstdarg>
 
 
-
-union Data {
-   int i;
-   float f;
-   bool b;
-   String* s;
-};  
-
+/**
+ * A Data Frame is a collection of columns with names rows
+ */
 class DataFrame : public Object {
     
     public:
-        DataFrame() {
+        /**
+         * Creates a dataframe with a length of the shortest column.
+         * The columns entered are now owned by this.
+         */
+        DataFrame(Column*...) {
+
+        }
+
+        /**
+         * Deletes all columns and names in this.
+         */
+        ~DataFrame() {
+
+        }
+
+        /**
+         * Names the row indicated by i, name. If i > the length, exits with
+         * code 1. The String entered is now owned by this.
+         */
+        void addRowName(String* name, size_t i) {
 
         }
 
@@ -50,6 +64,82 @@ class DataFrame : public Object {
         }
 
         /**
+         * Returns the float specified by row, col. If the index is out of bounds 
+         * or the column specified is not a FloatColumn, exits with
+         * error code 1.
+         */
+        float getFloat(size_t row, size_t col) {
+
+        }
+
+        /**
+         * Returns the data matching the row_name and col_name
+         * If there is no row_name or col_name matching the ones
+         * specified or the column specified is not a FloatColumn, 
+         * exits with error code 1
+         */
+        float getFloat(String* row_name, String* col_name) {
+
+        }
+
+        /**
+         * Returns the bool specified by row, col. If the index is out of bounds 
+         * or the column specified is not a BooleanColumn, exits with
+         * error code 1.
+         */
+        bool getBoolean(size_t row, size_t col) {
+
+        }
+
+        /**
+         * Returns the bool matching the row_name and col_name
+         * If there is no row_name or col_name matching the ones
+         * specified or the column specified is not a BooleanColumn, 
+         * exits with error code 1
+         */
+        bool getBoolean(String* row_name, String* col_name) {
+
+        }
+
+        /**
+         * Returns the string specified by row, col. If the index is out of bounds 
+         * or the column specified is not a StringColumn, exits with
+         * error code 1.
+         */
+        String* getString(size_t row, size_t col) {
+
+        }
+
+        /**
+         * Returns the string matching the row_name and col_name
+         * If there is no row_name or col_name matching the ones
+         * specified or the column specified is not a StringColumn, 
+         * exits with error code 1
+         */
+        String* getString(String* row_name, String* col_name) {
+
+        }
+
+        /**
+         * Returns the int specified by row, col. If the index is out of bounds 
+         * or the column specified is not a IntColumn, exits with
+         * error code 1.
+         */
+        int getInt(size_t row, size_t col) {
+
+        }
+
+        /**
+         * Returns the int matching the row_name and col_name
+         * If there is no row_name or col_name matching the ones
+         * specified or the column specified is not a IntColumn, 
+         * exits with error code 1
+         */
+        int getInt(String* row_name, String* col_name) {
+
+        }
+
+        /**
          * Returns the number of rows in the DataFrame
          */
         size_t nrow() {
@@ -61,6 +151,30 @@ class DataFrame : public Object {
          */
         size_t ncol() {
 
+        }
+
+        /**
+         * Returns if other is a dataframe with the same columns, and row names 
+         * as this
+         */
+        bool equals(Object* other) {
+
+        }
+
+        /**
+         * Returns the column type at index i or exits with error code 1 if i > 
+         * the number of columns
+         */
+        String* getColType(size_t i) {
+
+        }
+
+        /**
+         * Returns the column type of the column with the name given, or exits
+         * with error code 1 if no column has that name. 
+         */
+        String* getColType(String* name) {
+            
         }
 
 
--- assignment3/part2/columns.h
+++ assignment3/part2/columns.h
@@ -4,80 +4,184 @@
 #include "string.h"
 #include <cstdarg>
 
+union Data {
+   int i;
+   float f;
+   bool b;
+   String* s;
+}; 
+
+
 /**
- * 
+ * A Column is an Object that is meant to store data in a specific order.
+ * A Column has a name that can be changed.
  */
 class Column : public Object {
     public:
-
-        Column() { }
         /**
-         * returns the name of the column, or null if the column is not named
+         * Creates a column.
+         */
+        Column() { }
+
+        /**
+         * If there is a name associated with this column, deletes it.
+         */
+        ~Column() { }
+
+        /**
+         * Returns a new string with the name of the column, or null if the 
+         * column is not named.
          */
         String* getName() { }
 
         /**
-         * returns the length of the column
+         * Returns the length of the column.
          */
         size_t length() { }
 
         /**
-         * sets the name of the column to name
+         * Sets the name of the column to name.
          */
         void setName(String* name) { }
+
+        /**
+         * Determines if other is a column and it has the same name as this.
+         */
+        virtual bool equals(Object* other) { }
+
+        /**
+         * Returns an empty string.
+         */
+        virtual String* colType() { }
+
+        virtual Data get() { }
 };
 
+/**
+ * An int column is a column that holds ints.
+ */
 class IntColumn : public Column {
     public:
 
+        /**
+         * Creates an IntColumn with the ints specified.
+         */
         IntColumn(int...) : Column() { }
 
+
+        ~IntColumn() { }
+
         /**
-         * returns the int at the specified position, or exits
-         * with error code 1 if the i > the column length
+         * Returns the int at the specified position, or exits
+         * with error code 1 if the i > the column length.
          */
         int get(size_t i) { }
+
+        /**
+         * Determines if other is a IntColumn and it has the same name and ints in
+         * the same order as this.
+         */
+        virtual bool equals(Object* other) { }
+
+        /**
+         * Returns "Int".
+         */
+        virtual String* colType() { }
 };
 
 
-
+/**
+ * A StringColumn is a Column that holds Strings.
+ */
 class StringColumn : public Column {
     public:
 
+        /**
+         * Creates a StringColumn with the Strings specified.
+         */
         StringColumn(String*...) : Column() { }
+
+        ~StringColumn() { }
 
         /**
          * returns the String at the specified position, or exits
-         * with error code 1 if the i > the column length
+         * with error code 1 if the i > the column length.
          */
         String* get(size_t i) { }
 
+        /**
+         * determines if other is a StringColumn and it has the same name and 
+         * Strings in the same order as this.
+         */
+        virtual bool equals(Object* other) { }
+
+        /**
+         * returns "String".
+         */
+        virtual String* colType() { }
+
 };
 
-
+/**
+ * A BooleanColumn is a Column that holds booleans.
+ */
 class BooleanColumn : public Column {
     public:
 
+        /**
+         * Creates a BooleanColumn with the booleans specified.
+         */
         BooleanColumn(bool...) : Column() { }
+
+        ~BooleanColumn() { }
 
 
         /**
          * returns the boolean at the specified position, or exits
-         * with error code 1 if the i > the column length
+         * with error code 1 if the i > the column length.
          */
         bool get(size_t i) { }
 
+        /**
+         * determines if other is a BooleanColumn and it has the same name and 
+         * booleans in the same order as this.
+         */
+        virtual bool equals(Object* other) { }
+
+        /**
+         * returns "Boolean".
+         */
+        virtual String* colType() { }
+
 };
 
+/**
+ * A FloatColumn is a Column that holds Floats.
+ */
 class FloatColumn : public Column {
     public:
-
+        /**
+         * Creates a FloatColumn with the floats specified.
+         */
         FloatColumn(float...) : Column() { }
+
+        ~FloatColumn() {}
 
         /**
          * returns the float at the specified position, or exits
-         * with error code 1 if the i > the column length
+         * with error code 1 if the i > the column length.
          */
         float get(size_t i) { }
+
+        /**
+         * determines if other is a FloatColumn and it has the same name and 
+         * floats in the same order as this.
+         */
+        virtual bool equals(Object* other) { }
+
+        /**
+         * returns "Float".
+         */
+        virtual String* colType() { }
 
 };
